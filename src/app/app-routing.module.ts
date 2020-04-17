f5f6db12bdc99fb87ce7dced01d37637c4885c4f
Danilo Xavier <xavier@quatervoistecnologia.com.br>
2020-04-17 01:23:26 -0300

    Tela histórico de vendas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginComponentModule)
+  },
+  {
+    path: 'sales-history',
+    loadChildren: () => import('./sales-history/sales-history.module').then( m => m.LoginComponentModule)
   }
 ];
 
---
+++ src/app/sales-history/sales-history.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { SalesHistoryComponent } from './sales-history.component';
+
+describe('SalesHistoryComponent', () => {
+  let component: SalesHistoryComponent;
+  let fixture: ComponentFixture<SalesHistoryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SalesHistoryComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SalesHistoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
